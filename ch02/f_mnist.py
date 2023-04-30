919cd4a61e39051e9dc64f868d25b6bbe97388ad
Ericky <ericky121@gmail.com>
2023-05-01 00:20:22 +0800

    fashion mnist 완료

--- ch02/f_mnist.py
+++ ch02/f_mnist.py
@@ -2,7 +2,41 @@ import tensorflow as tf
 
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.python.keras.callbacks import Callback
+
+class myCallback(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') > 0.95):
+            print('\n정확도 95%에 도달하여 훈련을 멈춥니다.')
+            self.model.stop_training = True
+
+callbacks = myCallback()
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 
-f
+print(x_train.shape, y_train.shape)
+
+print(y_train[:10])
+
+x_train = x_train / 255.
+x_test = x_test / 255.
+
+model = Sequential([
+    Flatten(input_shape=[28, 28]),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax'),
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(
+    x_train, y_train,
+    epochs=100,
+    callbacks=[callbacks]
+)
+
+model.evaluate(x_test, y_test)
+
+classifications = model.predict(x_test)
+print(classifications[0])
+print(y_test[0])
